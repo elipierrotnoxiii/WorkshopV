96ab0e883f26b260d5e7bdc340d908a95a0909be
Eliezer Rodriguez <97846909+elipierrotnoxiii@users.noreply.github.com>
2025-10-20 21:30:46 -0300

    implementatio of the card prefab

---
+++ Assets/Scripts/Views/CardView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using TMPro;
+
+public class CardView : MonoBehaviour
+{
+    public TMP_Text title;
+    public TMP_Text description;
+    public TMP_Text mana;
+    public SpriteRenderer imageSR;
+    public GameObject wrapper;
+    
+}
