6bf08e7f38005cfffa5fdfe570922a36a5ab207f
DaCoH <daniel64167@gmail.com>
2025-12-01 21:23:29 -0300

    Perks System

--- Assets/Scripts/Used/Systems/MatchSetupSystem.cs
+++ Assets/Scripts/Used/Systems/MatchSetupSystem.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class MatchSetupSystem : MonoBehaviour
 {
     [SerializeField] private HeroData heroData;
+    [SerializeField] private PerkData perkData;
     [SerializeField] private List<EnemyData> enemyDatas;
     private void Start()
     {
         HeroSystem.Instance.Setup(heroData);
         EnemySystem.Instance.Setup(enemyDatas);
         CardSystem.Instance.Setup(heroData.Deck);
+        PerkSystem.Instance.AddPerk(new Perk(perkData));
         DrawCardsGA drawCardsGA = new(5);
         ActionSystem.Instance.Perform(drawCardsGA);
     }
